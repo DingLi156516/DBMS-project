10e48e11ad6a740cc283f9acca601910d6140f32
Ravi Krishnan Iyer <iyer.rav@husky.neu.edu>
2017-12-11 04:44:11 -0500

    ps

--- Trainly.io/login.php
+++ Trainly.io/login.php
@@ -17,32 +17,44 @@ if ($conn->connect_error) {
 $email_enter =  $_POST['email_id'];
 $pass_enter = $_POST['pass'];
 // Taking salt from the database
-$sqlsalt= "SELECT Salt from UserPasswords WHERE Userid = (Select Userid FROM UserInfo WHERE Email= '$email_enter') ";
-$resultsalt = $conn->query($sqlsalt);
-$saltarray = mysqli_fetch_array($resultsalt);
+$sqlsalt= "SELECT Salt from UserPasswords WHERE Userid = (Select Userid FROM UserInfo WHERE Email= ?) ";
+$resultsalt = $conn->prepare($sqlsalt);
+$resultsalt->bind_param('s',$email_enter);
+$resultsalt->execute();
+$result=$resultsalt->get_result();
+$saltarray=$result->fetch_assoc();
 $saltvalue = $saltarray['Salt'];
 
 $newpass = $saltarray['Salt'].$pass_enter;
 
 
-if($resultsalt -> num_rows > 0) {   
+if($result -> num_rows > 0) {   
     $pass_hash= hash('SHA256', $newpass);
 
-    $uid = "SELECT Userid from userinfo where email= '$email_enter' ";
-    $resultusers = $conn->query($uid); 
-    $userarray = mysqli_fetch_array($resultusers);
+    $uid = "SELECT Userid from userinfo where email= ? ";
+    $resultsalt1 = $conn->prepare($uid);
+    $resultsalt1->bind_param('s',$email_enter);
+    $resultsalt1->execute();
+    $result=$resultsalt1->get_result();
+    $userarray=$result->fetch_assoc();
     $useridval = $userarray['Userid'];
-    $sqlpass = "SELECT Password FROM userpasswords where Userid = '$useridval' ";
-    $resultpass = $conn->query($sqlpass);
-    $passarray = mysqli_fetch_array($resultpass);
+    $sqlpass = "SELECT Password FROM userpasswords where Userid = ? ";
+    $resultsalt2 = $conn->prepare($sqlpass);
+    $resultsalt2->bind_param('s',$useridval);
+    $resultsalt2->execute();
+    $result=$resultsalt2->get_result();
+    $passarray=$result->fetch_assoc();
     $userpass = $passarray['Password'];
 
     // Comparing password entered by the user with the password in database
     if (strtolower($userpass) == strtolower($pass_hash)){
         
-        $sqlall= " Select * from UserInfo WHERE Email='$email_enter'";
-        
-        $resultallusers = $conn->query($sqlall);
+        $sqlall= " Select * from UserInfo WHERE Email=?";
+        $user = $conn->prepare($sqlall);
+        $user->bind_param('s',$email_enter);
+        $user->execute();
+     
+        $resultallusers=$user->get_result();
 
         $alluserarray = $resultallusers->fetch_assoc();
         
